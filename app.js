7f75dde912eb38002c430ccacf07280878372a09
角田真雄 <zhenxiongjiaotian@gmail.com>
2020-04-17 01:03:40 +0900

    #1 実装

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let TodoApp = {
     addTask_tx: document.querySelector('.addTask_tx'),
     alert: document.querySelector('.alert'),
     addBtn: document.querySelector('.addBtn-wra'),
+    todos_wra: document.querySelector('.ul'),
     formCheck: function( task ) {
         //0　〜　16文字
         let checkTask = task;
@@ -17,13 +18,17 @@ let TodoApp = {
                 console.log('文字数oosugi');
                 TodoApp.alert.style.display = 'none';
             } else {
+                //チェックされた値が次の関数に渡されている。
                 TodoApp.createTodoList(checkTask);
                 TodoApp.alert.style.display = 'none';
+                TodoApp.modal.style.display = 'none';
             }
         }
         return innerCheckForm();
     },
     createTodoList: function( checkTask ) {
+        const html = `<li class="tast-item" ><p class="task_tx">${ checkTask }</p><i class="fas fa-trash-alt"></i></li>`;
+        TodoApp.todos_wra.innerHTML += html;
         console.log( checkTask );
     }
 }
